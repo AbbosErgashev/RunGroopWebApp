a2e6d805ae231a62dac260e98f7038e94776f155
eaxusniddinovich <eaxusniddinovich@gmail.com>
2024-03-20 08:25:17 +0500

    Add project files.

---
+++ RunGroopWebApp/Migrations/20240319131937_InitialCreate1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RunGroopWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReceCategory",
+                table: "Races",
+                newName: "RaceCategory");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RaceCategory",
+                table: "Races",
+                newName: "ReceCategory");
+        }
+    }
+}
